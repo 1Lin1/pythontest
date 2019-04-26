40088f9937f707a97bad994a3ace63992fda219a
LIN <624965986@qq.com>
2019-04-26 21:48:22 +0800

    4.26学习类和对象

--- test1.py
+++ test1.py
@@ -150,14 +150,188 @@
 # print (L2)
 
 # #生成器g 用()表示 打印不能直接用g
-g=(x*x for x in range(1,10))
-for i in g:
-    print(i,end=" ")
+# g=(x*x for x in range(1,10))
+# for i in g:
+#     print(i,end=" ")
 
-#生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator
-#把list、dict、str等Iterable变成Iterator可以使用iter()函数
+# #生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator
+# #把list、dict、str等Iterable变成Iterator可以使用iter()函数
 
-from collections import Iterator
-print (isinstance(iter([]),Iterator))
+# print (isinstance(iter([]),Iterator))
 
 
+
+#类和对象
+
+# class Person:
+#     def __init__(self,name,age):
+#        self.name=name
+#        self.age=age
+#     def say_hi(self):
+#         print ('你好 我叫',self.name)
+# p1=Person("Jack",18)
+# p1.say_hi()
+#类似java中的类和对象
+
+# class Person2:
+#     count=0
+#     name='Person'
+# Person2.count+=1
+# print (Person2.count)
+# print (Person2.name)
+# #类属性，静态属性
+
+# class Person11:
+#     def __init__(self,name):
+#         self.__name=name
+#     @property
+#     def name (self):
+#         """lock"""
+#         return self.__name
+
+# p=Person11('王五')
+# print (p.name)
+# #property 的用法
+
+# class Person12:
+#     def say_hi(self,name):
+#         self.name=name
+#         print ('我叫'+self.name)
+
+# p=Person12()
+# p.say_hi('hah')
+
+# class Foo:
+#     className='FOO'
+#     def __init__(self,name):#构造方法
+#         self.name=name
+#     def f1(self):   #实例方法
+#         print (self.name)
+#     @staticmethod
+#     def f2(): #静态方法
+#         print ('static')
+#     @classmethod
+#     def f3(cls):#类方法
+#         print (cls.className)
+# f=Foo('林')
+# f.f1()
+# Foo.f2()
+# Foo.f3()
+
+#在python中可以定义多个重名的方法，但只有最后一个生效
+
+# class Person():
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def say_hi(self):
+#         print ("你好 我叫{0},今年{1}岁了".format(self.name,self.age))
+# class student(Person):
+#     def __init__(self,name,age,stu_id):
+#         Person.__init__(self,name,age) 
+#         #重点:声明派生类，必须在其构造函数调用基类的构造函数
+#         self.stu_id=stu_id
+#     def say_hi(self):
+#         Person.say_hi(self)
+#         print ("我的学号是{0}".format(self.stu_id))
+
+# p=Person("林洹",18)
+# p.say_hi()
+# p2=student("hoyutim",17,201710089069)
+# p2.say_hi()
+
+# class Dime:
+#     def __init__(self,x,y):
+#         self.x=x
+#         self.y=y
+#     def area(self):
+#         pass
+# class Retangle:
+#     def __init__(self,w,h):
+#         Dime.__init__(self,w,h)
+#     def area(self):
+#         return self.x*self.y
+# d1=Retangle(3.0,4.0)
+# print (d1.area())
+
+#4.26
+
+# class Student():
+#     def __init__(self,name,score):
+#         self.name=name
+#         self.score=score
+# #self指向实例本身
+#     def get_grade(self):
+#         if self.score>=90:
+#             return 'A'
+#         elif self.score>=60:
+#             return 'B'
+#         else:
+#             return 'C'
+# a=Student('Lisa',99)
+# b=Student('Nna',56)
+# print (a.get_grade())
+# print (b.get_grade())
+
+# class Student2():
+#     def __init__(self,name,age):
+#         self.__name=name
+#         self.__age=age
+#     def print_age(self):
+#         print ('%s:%s'%(self.__name,self.__age))
+#     def set_age(self,age):
+#         self.__age=age
+#     def get_age(self):
+#         return self.__age
+# a=Student2('LISA',12)
+# b=Student2('nan',13)
+# a.print_age()
+# b.print_age()
+# print (a.get_age())
+# b.set_age(133)
+# print (b.get_age())
+
+#__name私有属性
+
+class Animal:
+    def run(self):
+        print ('Animal is running')
+
+class Dog(Animal):
+    def run(self):
+        print ('dog is running')
+        #重写继承的方法
+    def eat(self):
+        print ('dog is eat')
+
+class Cat(Animal):
+    def run(self):
+        print ('cat is running')
+          #重写继承的方法
+    def eat(self):
+        print ('cat is eat')
+class Timer():
+    def run(self):
+        print ('dada')
+def run_twice(animal):
+    animal.run()
+    animal.run()
+d=Dog()
+run_twice(Dog())
+run_twice(Timer())#python是动态语言，只要有run方法，都能继承
+print  (isinstance(d,Dog)) #用isinstanceo判断类型
+
+# 总是优先使用isinstance()判断类型，可以将指定类型及其子类“一网打尽”。
+
+print (dir(d))#获得一个对象的所有属性和方法
+
+print (hasattr(d,'x'))#是否有x属性
+setattr(d,'x',100) #设置x属性
+setattr(d,'y',200)
+print (getattr(d,'x'))  #获取x属性
+print (getattr(d,'y',404))#获取y属性，若不存在返回404
+fn=getattr(d,'x')
+print (fn)  
+
+sum=d.x+d.y
+print (sum)
